04570fc021a2a4c4625a4c38f332a253c67499ac
Ben Urbana <benurbana1@gmail.com>
2017-09-08 10:41:40 -0600

    Add function to find how many brown eyes

--- students.rb
+++ students.rb
@@ -1,6 +1,7 @@
 students = []
 eye_colors = []
 ages = []
+brown_eyes = 0
 
 File.open("student_data.csv").each do |line|
 	info = line.split(",")
@@ -9,6 +10,14 @@ File.open("student_data.csv").each do |line|
 	ages.push(info[2].strip.to_i)
 end
 
+eye_colors.each do |color|
+	if color == "Brown"
+		brown_eyes = brown_eyes + 1
+	end
+end
+
 print students, "\n"
 print eye_colors, "\n"
 print ages, "\n"
+print "\n"
+print brown_eyes
